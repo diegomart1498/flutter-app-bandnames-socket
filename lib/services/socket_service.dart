0093f087f74be45c16f01a60a3d324860b091d80
Diego Martinez <diegomart@unicauca.edu.co>
2023-02-21 22:14:52 -0500

    Servidor desplegado en Render

--- lib/services/socket_service.dart
+++ lib/services/socket_service.dart
@@ -22,7 +22,8 @@ class SocketService with ChangeNotifier {
 
   void _initConfig() {
     //!Dart client
-    _socket = IO.io('http://192.168.1.8:3000', {
+    // http://192.168.1.8:3000
+    _socket = IO.io('https://flutter-socket-server-m6t2.onrender.com', {
       'transports': ['websocket'],
       'autoConnect': true,
     });
